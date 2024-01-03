8cb9807fcba1cd88d48d0fec1161f63d28019998
FeBonifacio <103700361+FeBonifacio@users.noreply.github.com>
2024-01-03 14:08:38 -0300

    Terminando o middleware e login

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express';
 
 import { CreateUserController } from './controllers/user/CreateUserController';
-import { AuthUserController } from './controllers/user/AuthUserController'; 
+import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
+
+import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
 
 // Rotas user
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
+router.get('/me', isAuthenticated, new DetailUserController().handle)
+
 
 export { router };
