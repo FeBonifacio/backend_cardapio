42db66edb57fbf88e62b1a655302d8fe29bbbd45
FeBonifacio <103700361+FeBonifacio@users.noreply.github.com>
2024-01-03 15:55:05 -0300

    Categoria toda criada, nova e lista

--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,10 @@ import { Router } from 'express';
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
+import { ListCategoryController } from './controllers/category/ListCategoryController';
 
 const router = Router();
 
@@ -12,6 +14,12 @@ const router = Router();
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+// Rotas category
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+// Rotas produtos
 
 
 export { router };
