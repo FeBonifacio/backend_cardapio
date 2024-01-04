60002bf314a0b531659a776fefe4f3ac28138365
Fernando <fe.bonifacio@hotmail.com>
2024-01-04 11:05:26 -0300

    Finalizado abrir e fechar mesa

--- src/routes.ts
+++ src/routes.ts
@@ -12,6 +12,8 @@ import { CreateProductController } from './controllers/product/CreateProductCont
 
 import uploadConfig from './config/multer';
 import { ListByCategoryController } from './controllers/product/ListByCategoryController';
+import { CreateOrderController } from './controllers/order/CreateOrderController';
+import { RemoveOrderController } from './controllers/order/RemoveOrderController';
 
 const router = Router();
 
@@ -30,6 +32,10 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle)
 // Rotas produtos
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
+
+// Rotas Order
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 
 export { router };
