8575924390624638e681d49f0cfb7c6b9030702f
Fernando <fe.bonifacio@hotmail.com>
2024-01-04 15:07:27 -0300

    Finalizado o pedido e a exclusao do pedido

--- src/routes.ts
+++ src/routes.ts
@@ -14,6 +14,8 @@ import uploadConfig from './config/multer';
 import { ListByCategoryController } from './controllers/product/ListByCategoryController';
 import { CreateOrderController } from './controllers/order/CreateOrderController';
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
+import { AddItemController } from './controllers/order/AddItemController';
+import { RemoveItemController } from './controllers/order/RemoveItemController';
 
 const router = Router();
 
@@ -36,6 +38,8 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 // Rotas Order
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
+router.post('/order/add', isAuthenticated, new AddItemController().handle)
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
 
 
 export { router };
